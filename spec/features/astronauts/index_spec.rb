b514c580952623f7349ffa41a613f0d8cb3c795f
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-23 20:46:18 -0600

    Complete US 2

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'astronaut index' do
-  it 'lists all of the astronaut names' do
-    armstrong = Astronaut.create!(name: 'Neil Armstrong', age: 37, job: 'Commander')
-    aldrin = Astronaut.create!(name: 'Buzz Aldrin', age: 42, job: 'Navigator')
+  before :each do
+    @armstrong = Astronaut.create!(name: 'Neil Armstrong', age: 37, job: 'Commander')
+    @aldrin = Astronaut.create!(name: 'Buzz Aldrin', age: 43, job: 'Navigator')
+  end
 
+  it 'lists all of the astronaut names' do
     visit '/astronauts'
 
     expect(page).to have_content('Neil Armstrong')
@@ -12,23 +14,23 @@ RSpec.describe 'astronaut index' do
   end
 
   it 'lists the age and job of each astronaut' do
-    armstrong = Astronaut.create!(name: 'Neil Armstrong', age: 37, job: 'Commander')
-    aldrin = Astronaut.create!(name: 'Buzz Aldrin', age: 42, job: 'Navigator')
-
     visit '/astronauts'
 
-    within "div#astro-#{armstrong.id}" do
+    within "div#astro-#{@armstrong.id}" do
       expect(page).to have_content('Age: 37')
       expect(page).to have_content('Job: Commander')
     end
+  end
+
+  it 'lists the average age of all astronauts' do
+    visit '/astronauts'
+
+    expect(page).to have_content('Average Astronaut Age: 40')
   end
 end
 
 # As a visitor,
 # When I visit '/astronauts'
-# I see a list of astronauts with the following info:
-# - Name
-# - Age
-# - Job
+# I see the average age of all astronauts.
 #
-# (e.g. "Name: Neil Armstrong, Age: 37, Job: Commander")
+# (e.g. "Average Age: 34")
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,15 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many(:missions).through(:astronaut_missions)}
   end
+
+  describe 'class methods' do
+    describe '::average_age' do
+      it 'returns the average age of all astronauts' do
+        armstrong = Astronaut.create!(name: 'Neil Armstrong', age: 37, job: 'Commander')
+        aldrin = Astronaut.create!(name: 'Buzz Aldrin', age: 43, job: 'Navigator')
+
+        expect(Astronaut.average_age).to eq 40
+      end
+    end
+  end
 end
